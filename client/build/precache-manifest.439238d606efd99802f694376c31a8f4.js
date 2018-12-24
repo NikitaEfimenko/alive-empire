f06728faf00d355a0cbd6b539f0c5141d4d04d2a
Nikita Efimenko <mszx2000@gmail.com>
2018-12-24 06:53:34 +0400

    msc

---
+++ client/build/precache-manifest.439238d606efd99802f694376c31a8f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c33d34699e4ff55094db5a5d6e9bd243",
+    "url": "/static/media/font3.c33d3469.otf"
+  },
+  {
+    "revision": "66d392d15072363ecedee7ccfa3a4976",
+    "url": "/static/media/font1.66d392d1.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e2ad1c1981717729fba8",
+    "url": "/static/js/main.e2ad1c19.chunk.js"
+  },
+  {
+    "revision": "3c63c582b7648f1180a3",
+    "url": "/static/js/1.3c63c582.chunk.js"
+  },
+  {
+    "revision": "e2ad1c1981717729fba8",
+    "url": "/static/css/main.c32f9c4c.chunk.css"
+  },
+  {
+    "revision": "3c63c582b7648f1180a3",
+    "url": "/static/css/1.498450b1.chunk.css"
+  },
+  {
+    "revision": "8b9843ffd37ab742a81393e89ef8b24f",
+    "url": "/index.html"
+  }
+];
